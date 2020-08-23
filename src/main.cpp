e7d732cc6fe58a7f7cf563db092008cbeec056b5
Ivan Orfanidi <i.orfanidi@mail.ru>
2020-08-24 01:10:31 +0300

    Пример почтового клиента. Отправка письма на email.

---
+++ src/main.cpp
@@ -0,0 +1,41 @@
+﻿// on Ubuntu: sudo apt-get install libpoco-dev
+// or see http://pocoproject.org/
+// compile with: g++ -Wall -O3 send-mail-cxx.C -lPocoNet -lPocoFoundation
+
+#include <Poco/Net/MailMessage.h>
+#include <Poco/Net/SMTPClientSession.h>
+#include <cstdlib>
+#include <iostream>
+#include <string>
+
+namespace config {
+constexpr std::string_view SMTP_SERVER_NAME = "smtp-spb-00.obit.ru";
+}
+
+int main(int argc, char** argv)
+{
+    try {
+        Poco::Net::MailMessage msg;
+
+        msg.addRecipient(Poco::Net::MailRecipient(Poco::Net::MailRecipient::PRIMARY_RECIPIENT,
+            "i.orfanidi@mail.ru",
+            "Иван Орфаниди"));
+
+        msg.setSender("Ivan Orfanidi <i.orfanidi@mail.ru>");
+        msg.setSubject("Raspberry Pi");
+
+        msg.setContent("Sending mail from C++ using POCO C++ Libraries");
+
+        Poco::Net::SMTPClientSession smtp(config::SMTP_SERVER_NAME.data()); // SMTP server name
+        smtp.open();
+        smtp.login();
+        smtp.sendMessage(msg);
+        smtp.close();
+        std::cout << "Sent mail successfully!" << std::endl;
+    } catch (std::exception& e) {
+        std::cerr << "failed to send mail: " << e.what() << std::endl;
+        return EXIT_FAILURE;
+    }
+
+    return EXIT_SUCCESS;
+}
